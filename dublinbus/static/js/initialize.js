132e34d4fb9cd060485a6992d2fa72dcb69d0627
Evan McDonagh <evan.mc-donagh@ucdconnect.ie>
2020-06-23 13:02:06 +0100

    Added infowindow with serving bus routes

--- dublinbus/static/js/initialize.js
+++ dublinbus/static/js/initialize.js
@@ -14,7 +14,8 @@
 
     // map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
 
-
+var infowindow;
+var map;
 
 function initialize() {
         var mapProp = {
@@ -23,21 +24,40 @@ function initialize() {
             mapTypeId: google.maps.MapTypeId.ROADMAP
         };
         
-        var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
+        map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
+
+        infowindow = new google.maps.InfoWindow();
 
         stopKeys = Object.keys(stopdata);
-        console.log(stopdata);
 
+        // Add stop markers with onclick
         var marker;
+        var stopKey;
         for (i=0;i<stopKeys.length;i++) {
+            stopKey = stopKeys[i]
             marker = new google.maps.Marker({
                                     position: new google.maps.LatLng(
-                                        stopdata[stopKeys[i]]['latitude'], 
-                                        stopdata[stopKeys[i]]['longitude']),
+                                        stopdata[stopKey]['latitude'], 
+                                        stopdata[stopKey]['longitude']),
                                     map: map
                                 });
+            marker.addListener('click', (function (marker, stopKey) {
+                return function () {
+                    getStopInfo(marker, stopKey)
+                                    }
+                })(marker, stopKey));
         }
-        
+
         marker.setMap(map);
     };
     google.maps.event.addDomListener(window, 'load', initialize);
+
+function getStopInfo(marker, stopKey) {
+    let content = "<div id='infowindow'><h5>Stop ID: " + stopdata[stopKey]["id"] + "</h5>";
+    content += "Routes: " + stopdata[stopKey]['routes'];
+    content += "</div>"
+
+    // add content of get request to info window
+    infowindow.setContent(content);
+    infowindow.open(map, marker);
+}
