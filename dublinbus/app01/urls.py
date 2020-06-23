6ae92c0121f05eb023ace97bf4b7fc482d5fec1a
yiyuan <yiyuan.geng@ucdconnect.ie>
2020-06-23 17:49:24 +0100

    add leapcard feature

--- dublinbus/app01/urls.py
+++ dublinbus/app01/urls.py
@@ -4,5 +4,6 @@ from app01 import views
 
 urlpatterns = [
     url('index/', views.index),
-    url('', views.index),
+    # url('', views.index),
+    url('leapcard/',views.leapcard),
 ]
--- dublinbus/app01/views.py
+++ dublinbus/app01/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render,HttpResponse
-
+from app01 import models
+from .forms import leapForm,routeForm
+from pyleapcard import *
+from pprint import pprint
+import json
+import requests
 # Create your views here.
 def index(request):
     context = load_bus_data()
@@ -21,3 +26,35 @@ def load_bus_data():
     return context
 
 # load_bus_data()
+def leapcard(request):
+
+    if request.method == 'POST':
+        form = leapForm(request.POST)
+        # print(form)
+        if form.is_valid():
+            name = form.cleaned_data['username']
+            pwd = form.cleaned_data['password']
+            context = {}
+            # print(name,pwd)
+            try:
+                session = LeapSession()
+                session.try_login(name,pwd)
+                overview = session.get_card_overview()
+                # print(overview)
+                leap_content = vars(overview)
+                context['card_num'] = leap_content.get('card_num')
+                context['balance'] = leap_content.get('balance')
+                # context['leap_content'] = leap_content
+
+
+            except:
+                context['wrong'] = "The user or the password is wrong, please try again"
+                print("the wrong password")
+            finally:
+                context['form'] = form
+                return render(request,'leapcard.html', context = context)
+
+
+
+    form = leapForm()
+    return render(request,'leapcard.html', {'form' : form})
