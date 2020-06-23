16d57060a7ebfecff868412b7011812547e3e62e
Evan McDonagh <evan.mc-donagh@ucdconnect.ie>
2020-06-23 09:51:25 +0100

    Added all Dublin Bus and Go-Ahead markers

--- dublinbus/app01/views.py
+++ dublinbus/app01/views.py
@@ -2,4 +2,22 @@ from django.shortcuts import render,HttpResponse
 
 # Create your views here.
 def index(request):
-    return render(request,'index.html')
+    context = load_bus_data()
+    return render(request,'index.html', context)
+
+def load_bus_data():
+    #load bus stop and route data
+    import json
+    import os
+    dirname = os.path.dirname(__file__)
+    stopfile = os.path.join(dirname, "../local-bus-data/stop-data.json")
+    routefile = os.path.join(dirname, "../local-bus-data/route-data.json")
+    with open(stopfile) as infile:
+        stop_data = json.load(infile)
+    with open(routefile) as infile:
+        route_data = json.load(infile)
+    stopdump = json.dumps(stop_data)
+    context = {'stopdata':stopdump}
+    return context
+
+# load_bus_data()
--- dublinbus/static/js/initialize.js
+++ dublinbus/static/js/initialize.js
@@ -22,6 +22,22 @@ function initialize() {
             zoom: 13,
             mapTypeId: google.maps.MapTypeId.ROADMAP
         };
+        
         var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
-    }
+
+        stopKeys = Object.keys(stopdata);
+        console.log(stopdata);
+
+        var marker;
+        for (i=0;i<stopKeys.length;i++) {
+            marker = new google.maps.Marker({
+                                    position: new google.maps.LatLng(
+                                        stopdata[stopKeys[i]]['latitude'], 
+                                        stopdata[stopKeys[i]]['longitude']),
+                                    map: map
+                                });
+        }
+        
+        marker.setMap(map);
+    };
     google.maps.event.addDomListener(window, 'load', initialize);
