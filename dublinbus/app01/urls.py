ea6ce514a8ae1e178cbfc3564ee8a65e7b212cb0
lgs <leeguosheng@126.com>
2020-07-02 17:07:43 +0100

    Update urls.py

--- dublinbus/app01/urls.py
+++ dublinbus/app01/urls.py
@@ -6,8 +6,8 @@ from app01 import views
 urlpatterns = [
     # url('index/', views.index),
     path('^$', views.index),
-    path('stop/', views.stop),
-    path('leapcard/',views.leapcard),
-    path('real_info/',views.real_info),
-    path('twitter/',views.twitter),
+    path('stop', views.stop),
+    path('leapcard',views.leapcard),
+    path('real_info',views.real_info),
+    path('twitter',views.twitter),
 ]
