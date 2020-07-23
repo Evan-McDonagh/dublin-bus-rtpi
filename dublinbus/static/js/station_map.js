b54a1d9bc56255061abe2345926d7ebd2170b283
lgs <leeguosheng@126.com>
2020-07-23 18:08:59 +0100

    polyline bug fixed

--- dublinbus/static/js/station_map.js
+++ dublinbus/static/js/station_map.js
@@ -17,13 +17,17 @@ function getCookie(name) { //csrf verification
 var csrftoken = getCookie('csrftoken'); //store the cookie msg in "csrftoken"
 
 var origin_pos = {} //represent the origin position, lat, lng
+var destination_pos = {}//represent the destination position, lat, lng
 // var origin_value = ""; //the text in the input box with id "origin"
 
 var map;
 var nearMeList = [];
 var allstopmarkers = [];
 var nearmemarkers = [];
-var alongroutemarkers = []
+var alongroutemarkers = [];
+var ori_dest_markers = [];
+var directionsService = new google.maps.DirectionsService();
+var directionsRenderer = new google.maps.DirectionsRenderer();
 
 function initMap(){
     //Initialize the map when the page is loaded
@@ -77,9 +81,7 @@ function initMap(){
 
 }
 
-    
 // google.maps.event.addDomListener(window, 'load', initMap);
-
 
 //send the users location "pos" to backend function to get the address info at this location and display it on map
 function sendlocation(pos, map){
@@ -122,6 +124,7 @@ function realtimeweather(pos) {
     })
 }
 
+//deal with the geolocation error
 function handleLocationError(browserHasGeolocation, pos, map) {
     //supporting functions of initMap()
     alert(browserHasGeolocation ?
@@ -135,82 +138,79 @@ function handleLocationError(browserHasGeolocation, pos, map) {
     // infoWindow.open(map);
 }
 
-function search() {
-    //trigger relevant functions to generate directions
-    //query the place according to the input boxes.
-    clearmarkers(allstopmarkers);
-    clearmarkers(nearmemarkers);
-    if (document.getElementById('origin').value ==""){var s_origin = origin_value;}
-    else{var s_origin = document.getElementById('origin').value}
-    var s_dest = document.getElementById('destination').value;
-
-    // var map2 = new google.maps.Map(document.getElementById('googleMap'), {
-    //     center: origin_pos,
-    //     zoom: 13
-    // });
-    var service = new google.maps.places.PlacesService(map);
-    var s_origin_request = {
-        query: s_origin,
-        fields: ['name', 'geometry'],
-    };
-    service.findPlaceFromQuery(s_origin_request, function (results, status) {
-        if (status === google.maps.places.PlacesServiceStatus.OK) {
-            map.setCenter(results[0].geometry.location);
-            var infowindow_ori = new google.maps.InfoWindow({
-                content: s_origin,
-            });
-            var marker_ori = new google.maps.Marker({
-                map: map,
-                position: results[0].geometry.location,
-                // icon:'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png',
-
-            });
-            marker_ori.addListener("mouseover", function () {
-                infowindow_ori.open(map, marker_ori);
-            });
-            origin_pos = results[0].geometry.location;
-            // origin_pos = {'lat': results[0].geometry.location.lat(), 'lng': results[0].geometry.location.lng()}
-        }
-    });
-    var s_dest_request = {
-        query: s_dest,
-        fields: ['name', 'geometry'],
-    };
-    service.findPlaceFromQuery(s_dest_request, function(results, status) {
-          if (status === google.maps.places.PlacesServiceStatus.OK) {
-              map.setCenter(results[0].geometry.location);
-              var infowindow_dest = new google.maps.InfoWindow({
-                content: s_dest,
-                });
-              var marker_dest = new google.maps.Marker({
-                              map: map,
-                              position: results[0].geometry.location,
-                              // icon:'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png',
-              });
-              marker_dest.addListener("mouseover", function () {
-                    infowindow_dest.open(map, marker_dest);
-                });
-              destination_pos = {'lat':results[0].geometry.location.lat(), 'lng':results[0].geometry.location.lng()}
-      }
-    });
-}
+//trigger relevant functions to generate directions
+// function search() {
+//     clearmarkers(allstopmarkers);
+//     clearmarkers(nearmemarkers);
+//     //query the place according to the input boxes.
+//     if ($("#origin").val() == "" || $("#destination").val() == "") {alert('The "from" or "to" box cannot be empty')}
+//     else {
+//         var s_origin = document.getElementById('origin').value
+//         var s_dest = document.getElementById('destination').value;
+//
+//         var service = new google.maps.places.PlacesService(map);
+//
+//         //search the location of origin input
+//         var s_origin_request = {
+//             query: s_origin,
+//             fields: ['name', 'geometry'],
+//         };
+//         service.findPlaceFromQuery(s_origin_request, function (results, status) {
+//             if (status === google.maps.places.PlacesServiceStatus.OK) {
+//                 map.setCenter(results[0].geometry.location);
+//                 var infowindow_ori = new google.maps.InfoWindow({
+//                     content: s_origin,
+//                 });
+//                 var marker_ori = new google.maps.Marker({
+//                     map: map,
+//                     position: results[0].geometry.location,
+//                     icon:'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png',
+//
+//                 });
+//                 marker_ori.addListener("mouseover", function () {infowindow_ori.open(map, marker_ori);});
+//                 marker_ori.addListener("mouseout", function () {infowindow_ori.close(map, marker_ori);});
+//                 origin_pos = results[0].geometry.location;
+//                 ori_dest_markers.push(marker_ori);
+//                 // origin_pos = {'lat': results[0].geometry.location.lat(), 'lng': results[0].geometry.location.lng()}
+//             }
+//         });
+//
+//         //search the location of destination input
+//         var s_dest_request = {
+//             query: s_dest,
+//             fields: ['name', 'geometry'],
+//         };
+//         service.findPlaceFromQuery(s_dest_request, function (results, status) {
+//             if (status === google.maps.places.PlacesServiceStatus.OK) {
+//                 // map.setCenter(results[0].geometry.location);
+//                 var infowindow_dest = new google.maps.InfoWindow({
+//                     content: s_dest,
+//                 });
+//                 var marker_dest = new google.maps.Marker({
+//                     map: map,
+//                     position: results[0].geometry.location,
+//                     icon:'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png',
+//                 });
+//                 marker_dest.addListener("mouseover", function () {infowindow_dest.open(map, marker_dest);});
+//                 marker_dest.addListener("mouseout", function () {infowindow_dest.close(map, marker_dest);});
+//                 destination_pos = {'lat': results[0].geometry.location.lat(), 'lng': results[0].geometry.location.lng()};
+//                 ori_dest_markers.push(marker_dest);
+//             }
+//         });
+//     }
+// }
 
 function calcRoute() {
-  // var start = document.getElementById('origin').value;
-  // var end = document.getElementById('destination').value;
-  var directionsService = new google.maps.DirectionsService();
-  var directionsRenderer = new google.maps.DirectionsRenderer();
-  var request = {
-    origin: {query: document.getElementById('origin').value},
-    destination: {query: document.getElementById('destination').value},
-    travelMode: 'TRANSIT',
-    transitOptions:{departureTime: new Date(document.getElementById("datetimepicker1").value)},
-    provideRouteAlternatives: false,
-  };
-    // var map3 = new google.maps.Map(document.getElementById('googleMap'),{
-    //     center: origin_pos,
-    //     zoom:13,
-    // });
+    directionsRenderer.setMap(null);
+    clearmarkers(alongroutemarkers);
+    clearmarkers(nearmemarkers);
+      var request = {
+        origin: {query: document.getElementById('origin').value},
+        destination: {query: document.getElementById('destination').value},
+        travelMode: 'TRANSIT',
+        transitOptions:{departureTime: new Date(document.getElementById("datetimepicker1").value)},
+        provideRouteAlternatives: false,
+      };
     directionsRenderer.setMap(map);
     directionsService.route(request, function(result, status) {
     if (status == 'OK') {
@@ -224,17 +224,14 @@ function calcRoute() {
                 var bus_name = [];
                 var bus_name_str = '';
                 steps = legs[j]['steps']
-                // alert(result['routes'][i]['bounds'])
                 for (k in steps){
                     if (steps[k].travel_mode == 'WALKING'){walking_dur += steps[k]['duration'].value}
                     else if (steps[k].travel_mode == 'TRANSIT'){bus_dur += steps[k]['duration'].value; bus_name.push(steps[k]['transit']['line'].short_name)}
                 }
-                // alert(bus_name[0])
                 if (bus_name.length > 1){for (i in bus_name){bus_name_str += bus_name[i] + "->"}}
                 else{bus_name_str = bus_name[0]};
                 routes_dict[bus_name_str] = result['routes'][i];
                 document.getElementById('routes').innerHTML = "<button id="+bus_name_str + ">"+bus_name_str+"</button>" + "walk:" + walking_dur +"s, on bus:"+ bus_dur+"s<br>";
-                // loadstops(bus_name, result['routes'][i]['bounds'])
                 document.getElementById(bus_name_str).addEventListener('click', function(){loadstops(bus_name, result['routes'][i]['bounds'], map);});
 
             }
@@ -283,6 +280,7 @@ function calcRoute() {
    directionsRenderer.setPanel(document.getElementById('h51'));
 }
 
+//Find the nearme stops and generate a msg to describe them.
 function nearMe(ls){
     // var size = Object.keys(ls).length;
     var size = ls.length;
@@ -304,20 +302,21 @@ function nearMe(ls){
     } 
 }
 
+//To make the markers in a list invisible.
 function clearmarkers(markerlist){
     for (var i=0; i < markerlist.length; i++){
         markerlist[i].setMap(null);
     }
 }
+
+//To make the markers in a list visible.
 function showmarkers(markerlist, map) {
     for (var i=0; i < markerlist.length; i++){
-        // alert(i)
         markerlist[i].setMap(map);
-        // markerlist[i].addListener('click', (function (marker, stopKey) {
-        //         return function () {getStopInfo(marker, stopKey, map);}
-        //     })(marker, stopKey));
     }
 }
+
+//triggered by clicking the markers to show stop info.
 function getStopInfo(marker, stopKey) {
     infowindow = new google.maps.InfoWindow();
     let content = "<div id='infowindow'><h5>Stop Number: " + stopdata[stopKey]["stopno"] + "</h5>";
@@ -329,6 +328,7 @@ function getStopInfo(marker, stopKey) {
     infowindow.open(map, marker);
 }
 
+//Add all stops markers on the map.
 function addallmarkers(map) {
     var stopKeys = Object.keys(stopdata);
     for (var i=0;i<stopKeys.length;i++) {
@@ -348,6 +348,7 @@ function addallmarkers(map) {
     showmarkers(allstopmarkers, map);
 }
 
+//Add the markers of nearme stops and display them on map
 function addnearmemarkers(map, pos){
     var stopKeys = Object.keys(stopdata);
     var lngT = 0.005;
@@ -370,36 +371,32 @@ function addnearmemarkers(map, pos){
     nearMe(nearMeList);
     showmarkers(nearmemarkers, map);
 }
-function stopsearch() {
-    info = {csrfmiddlewaretoken: '{{ csrf_token }}', stop_id :  $('#stop_id').val()};
-        // console.log(info);
 
+// The function to search a single stopid in the map and display the time info.
+function stopsearch() {
+    directionsRenderer.setMap(null);
         $.ajax({
             headers: {'X-CSRFToken': csrftoken},
             type:"POST",
             url: "http://127.0.0.1:8000/stop/",
             cache: false,
-            dataType: "html",
-            // headers:{'Content-Type':"application/json"} ,
-            data:JSON.stringify($('#stop_id').val()),
+            dataType: "json",
+            data:{'stop_id':$('#stop_id').val()},
             success: function(result, statues, xml){
-                result = JSON.parse(result);
-                console.log(result['results']);
                 var real_info = "Time Table" +"<br>";
                 for (var i =0; i< result["results"].length; i++){
-                real_info += result["results"][i]["route"]+"        "+result["results"][i]["arrivaldatetime"] +"<br>";
-                    }
-                    // console.log(real_info);
+                    real_info += result["results"][i]["route"]+"        "+result["results"][i]["arrivaldatetime"] +"<br>";
+                }
                 $("#stoparea").html(real_info);
             },
             error: function(){
                 alert("false");
             }
         });
-
     var stop_id = $('#stop_id').val();
     stopKeys = Object.keys(stopdata);
     clearmarkers(alongroutemarkers);
+    clearmarkers(nearmemarkers);
     var marker;
     var markers = [];
     var stopKey;
