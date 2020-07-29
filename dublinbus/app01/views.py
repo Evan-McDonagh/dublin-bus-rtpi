ca240a7ef6447c131e10a05bed9197376e5ea043
lgs <leeguosheng@126.com>
2020-07-29 11:16:12 +0100

    Update views.py

--- dublinbus/app01/views.py
+++ dublinbus/app01/views.py
@@ -1,16 +1,19 @@
+import math
 import re
 
 from django.http import JsonResponse
-from django.shortcuts import render,HttpResponse
+from django.shortcuts import render, HttpResponse
 from app01 import models
-from .forms import leapForm,routeForm
+from .forms import leapForm, routeForm
 from pyleapcard import *
 from pprint import pprint
 import json
 import requests
 import re
+import os
 import datetime
 from app01 import get_prediction
+
 #  Google Map Apikey
 gmap_api = 'AIzaSyB_Bqco2DvRVp55QdFyANIiDRSKS8IE8p8'
 
@@ -20,16 +23,26 @@ weather_fore_api = '9570260da25526e20bf66bdf7e1c25e5'
 # OpenWeather Preent Apikey
 weather_pre_api = 'c9d5929c3180f174f633828540c0fbc5'
 
+dirname = os.path.dirname(__file__)
+stopfile = os.path.join(dirname, "../local-bus-data/stop-data.json")
+routefile = os.path.join(dirname, "../local-bus-data/all-routes-sequences.json")
+
+with open(routefile) as rt:
+    allroutes = json.load(rt)
+    rt.close()
+with open(stopfile) as st:
+    allstops = json.load(st)
+    st.close()
+
+
 # Create your views here.
 def index(request):
-    
     context = load_bus_data()
-    return render(request,'index.html', context)
+    return render(request, 'index.html', context)
+
 
 def load_bus_data():
-    #load bus stop and route data
-    import json
-    import os
+    # load bus stop and route data
     dirname = os.path.dirname(__file__)
     stopfile = os.path.join(dirname, "../local-bus-data/stop-data.json")
     routefile = os.path.join(dirname, "../local-bus-data/route-data.json")
@@ -38,7 +51,7 @@ def load_bus_data():
     with open(routefile) as infile:
         route_data = json.load(infile)
     stopdump = json.dumps(stop_data)
-    context = {'stopdata':stopdump}
+    context = {'stopdata': stopdump}
     return context
 
 
@@ -47,12 +60,12 @@ def leapcard(request):
         print(request.POST)
         username = request.POST['username']
         password = request.POST['password']
-        print(username,password)
+        print(username, password)
         context = {}
-        
+
         try:
             session = LeapSession()
-            session.try_login(username,password)
+            session.try_login(username, password)
             overview = session.get_card_overview()
             # print(overview)
             leap_content = vars(overview)
@@ -62,18 +75,20 @@ def leapcard(request):
         except:
             context['wrong'] = "The user or the password is wrong, please try again"
             print("the wrong password")
-            return JsonResponse(context,safe=False)
-        
-        return JsonResponse(context,safe=False)
+            return JsonResponse(context, safe=False)
+
+        return JsonResponse(context, safe=False)
+
 
 def stop(request):
     if request.method == 'POST':
         stop_id = request.POST['stop_id']
-        url = "https://data.smartdublin.ie/cgi-bin/rtpi/realtimebusinformation" +"?stopid=" + stop_id+"&format=json"
+        url = "https://data.smartdublin.ie/cgi-bin/rtpi/realtimebusinformation" + "?stopid=" + stop_id + "&format=json"
         obj = requests.get(url)
         obj_json = obj.json()
         print(obj_json)
         return JsonResponse(obj_json, safe=False)
+
 
 # get user address info by user's location
 def init(request):
@@ -83,14 +98,15 @@ def init(request):
         lng = request.POST.get('lng')
 
         # get the address of the post coordinate
-        address_request = requests.get('https://maps.googleapis.com/maps/api/geocode/json?latlng={},{}&key={}'.format(lat, lng, gmap_api))
+        address_request = requests.get(
+            'https://maps.googleapis.com/maps/api/geocode/json?latlng={},{}&key={}'.format(lat, lng, gmap_api))
         address_result = json.loads(address_request.text)
         address = address_result['results'][0].get('formatted_address')
         inifo['address'] = address
     else:
         inifo['address'] = 'location unknown'
     return HttpResponse(json.dumps(inifo))
-    
+
 
 # get realtime weather at the users' location
 def weather(request):
@@ -122,59 +138,177 @@ def weather(request):
 
 # select involved stops along the route.
 def printresult(request):
-    import os
-
     if request.method == 'POST':
-        rebody = json.loads(request.body)
-        # with open ('result.txt', 'w') as rt:
-        #     rt.write(str(rebody))
-        # rt.close()
-        bounds = rebody.get('bounds')
-        bus = rebody.get('bus')
+        # print(request.body)
+        SEGSINFO = json.loads(request.body)
+        seg_stops = {}
+        segments = SEGSINFO[0]
+        bounds = SEGSINFO[1]
+        for seg in segments:
+            if seg['travelmode'].upper() == 'TRANSIT':
+                busname = seg['busname']
+                alongroutestops = matchstop(seg, allstops)
+                alongroutestopinfos = []
+                # print(alongroutestops)
+                for stop in alongroutestops:
+                    for stopkey in allstops:
+                        if stop == stopkey:
+                            STOP = allstops[stopkey]
+                            if isInbounds(bounds, STOP):
+                                alongroutestopinfos.append(
+                                    {"id": STOP['stopno'], 'lat': STOP["latitude"], 'lng': STOP["longitude"]})
+                seg_stops[busname] = alongroutestopinfos
+            else:
+                continue
+        # print(seg_stops)
+        return JsonResponse(seg_stops, safe=False)
 
-        dirname = os.path.dirname(__file__)
-        stopfile = os.path.join(dirname, "../local-bus-data/stop-data.json")
-        routefile = os.path.join(dirname, "../local-bus-data/route-data.json")
 
-        with open(routefile) as rt:
-            allroutes = json.load(rt)
-            rt.close()
-        with open(stopfile) as st:
-            allstops = json.load(st)
-            st.close()
-        stop_locations = []
+# to match all stops to see if the stop is standing in the route.
+def matchstop(seg, allstops):
+    busname = seg['busname']
+    startstopname = seg['startstopname']
+    startstoplocation = seg['startstoplocation']
+    endstopname = seg['endstopname']
+    endstoplocation = seg['endstoplocation']
+    headsign = seg['headsign']
+    numstops = seg['numstops']
+    pattern = re.compile(r'(?<=stop )\d+\.?\d*')
+    alongroutestopids = []
+    if len(pattern.findall(startstopname)) > 0 and len(pattern.findall(endstopname)) > 0:
+        startstopno = pattern.findall(startstopname)[0]
+        endstopno = pattern.findall(endstopname)[0]
+        startstopkey = ""
+        endstopkey = ""
+        for stopkey in allstops:
+            if allstops[stopkey].get('stopno') == startstopno:
+                startstopkey = stopkey
+                continue
+            if allstops[stopkey].get('stopno') == endstopno:
+                endstopkey = stopkey
+                continue
+        for route in allroutes:
+            if route == busname:
+                ROUTE = allroutes[route]
+                for in_out in ROUTE:
+                    atcocodes = ROUTE[in_out]['atcocodes']
+                    # print(atcocodes)
+                    if startstopkey in atcocodes and endstopkey in atcocodes:
+                        startindex = atcocodes.index(startstopkey)
+                        endindex = atcocodes.index(endstopkey)
+                        if startindex <= endindex:
+                            alongroutestopids = atcocodes[startindex:endindex + 1]
+                            return alongroutestopids
+                        else:
+                            alongroutestopids = atcocodes[endindex:startindex + 1]
+                            return alongroutestopids
+                    else:
+                        continue
+    elif len(pattern.findall(startstopname)) > 0:
+        startstopno = pattern.findall(startstopname)[0]  # if startstopname contains stopno info
+        for stopkey in allstops:
+            if allstops[stopkey].get('stopno') == startstopno:
+                alongroutestopids += slicealongroutestopsid(stopkey, busname, numstops, "start")
+                # print('startno', alongroutestopids)
+                return alongroutestopids
+    elif len(pattern.findall(endstopname)) > 0:
+        endstopno = pattern.findall(endstopname)[0]
+        for stopkey in allstops:
+            if allstops[stopkey].get('stopno') == endstopno:
+                print(stopkey)
+                alongroutestopids += slicealongroutestopsid(stopkey, busname, numstops, "end")
+                # print('stopno', alongroutestopids)
+                return alongroutestopids
+    else:
+        distance = 0.01
+        longstopid = ""
+        routestopskeys = allroutes[busname]['Inbound']['atcocodes'] + allroutes[busname]['Outbound']['atcocodes']
+        routestops = {}
+        for stopkey in allstops:
+            for key in routestopskeys:
+                if stopkey == key:
+                    routestops[stopkey] = allstops[stopkey]
+        for stopkey in routestops:
+            if routestops[stopkey].get('latitude') is not None:
+                stop_loc = {'lat': routestops[stopkey].get('latitude'), 'lng': routestops[stopkey].get('longitude')}
+            else:
+                print(stopkey, "location does not exist, replaced with {0,0}")
+                stop_loc = {'lat': 0, 'lng': 0}
+            DIST = gettwostopdistance(startstoplocation, stop_loc);
+            if DIST >= distance:
+                continue
+            else:
+                distance = DIST
+                longstopid = stopkey
+        alongroutestopids += slicealongroutestopsid(longstopid, busname, numstops, "start")
+        return alongroutestopids
 
-        # route_stop_locations = {}
-        for i in bus:
-            bus_route = allroutes[i]
-            west = bounds['west']
-            east = bounds['east']
-            north = bounds['north']
-            south = bounds['south']
-            for stop in bus_route:
-                STOP = allstops[stop]
-                slat = STOP["latitude"]
-                slng = STOP["longitude"]
-                if (slat >= south and slat <= north) or (slat >= north and slat <= south):
-                    if (slng >= west and slng <= east) or (slng >= east and slng <= west):
-                        stop_locations.append({"id": STOP["stopno"], 'lat':slat, 'lng':slng})
-    return HttpResponse(json.dumps({'stop_locations':stop_locations}))
+
+#  to check if the stop in bounds
+def isInbounds(bounds, stop):
+    slat = stop['latitude']
+    slng = stop['longitude']
+    south = bounds['south']
+    north = bounds['north']
+    west = bounds['west']
+    east = bounds['east']
+    if (slat >= south and slat <= north) or (slat <= south and slat >= north):
+        if (slng >= west and slng <= east) or (slng <= west and slng >= east):
+            return True
+    return False
+
+
+#  to get the distance between two stops
+def gettwostopdistance(loc1, loc2):
+    lat_diff = loc1['lat'] - loc2['lat']
+    lng_diff = loc1['lng'] - loc2['lng']
+    dist = math.sqrt(pow(lat_diff, 2) + pow(lng_diff, 2))
+    return dist
+
+
+# to extract the sublist in a route sequence list
+def slicealongroutestopsid(longstopid, busname, numstops, start_or_end):
+    print("numstops", numstops)
+    for route in allroutes:
+        if route == busname:
+            ROUTE = allroutes[route]
+            for in_out in ROUTE:
+                atcocodes = ROUTE[in_out]['atcocodes']
+                for i in range(0, len(atcocodes)):
+                    if atcocodes[i] == longstopid:
+                        print('i', i)
+                        if start_or_end == 'start':
+                            if i + numstops + 1 <= len(atcocodes):
+                                alongroutestopids = atcocodes[i:i + numstops + 1]
+                                return alongroutestopids
+                            else:
+                                continue
+                        else:
+                            if i - numstops >= 0:
+                                alongroutestopids = atcocodes[i - numstops:i + 1]
+                                return alongroutestopids
+                            else:
+                                continue
+
 
 # show realtime info when a marker alongside the route is clicked
 def rtmarkerinfo(request):
     if request.method == 'POST':
         stop_id = request.POST.get('id')
-        url = "https://data.smartdublin.ie/cgi-bin/rtpi/realtimebusinformation" +"?stopid=" + stop_id+"&format=json"
+        url = "https://data.smartdublin.ie/cgi-bin/rtpi/realtimebusinformation" + "?stopid=" + stop_id + "&format=json"
         obj = requests.get(url)
         obj_json = obj.json()
         # print(stop_id)
-        allinfo = "Stop No." + obj_json.get('stopid') +"<br>"
-        rsp ={obj_json.get('stopid'): []}
+        allinfo = "Stop No." + obj_json.get('stopid') + "<br>"
+        rsp = {obj_json.get('stopid'): []}
         for result in obj_json['results']:
             key = result.get('route')
-            rsp[obj_json.get('stopid')].append({key: {'arrivaltime':result.get('arrivaldatetime'), 'destination':result.get('destination')}})
-            allinfo += "Route:"+ key + "  arrive at:" + result.get('arrivaldatetime') + " Towards " + result.get('destination') +"<br>"
-        return HttpResponse(json.dumps({"allinfo":allinfo}))
+            rsp[obj_json.get('stopid')].append(
+                {key: {'arrivaltime': result.get('arrivaldatetime'), 'destination': result.get('destination')}})
+            allinfo += "Route:" + key + "  arrive at:" + result.get('arrivaldatetime') + " Towards " + result.get(
+                'destination') + "<br>"
+        return HttpResponse(json.dumps({"allinfo": allinfo}))
+
 
 # show prediction
 def showprediction(request):
@@ -195,11 +329,12 @@ def showprediction(request):
                 stopA = int(seg['startstopno'])
                 stopB = int(seg['endstopno'])
 
-                try: 
-                    prediction = int(get_prediction.get_prediction(route,1,datestring,stopA,stopB))
+                try:
+                    prediction = int(get_prediction.get_prediction(route, 1, datestring, stopA, stopB))
                 except IndexError as e:
-                    prediction = int(get_prediction.get_prediction(route,2,datestring,stopA,stopB))
-            
+                    prediction = int(get_prediction.get_prediction(route, 2, datestring, stopA, stopB))
+
                 predictions += [prediction]
     print(predictions)
     return HttpResponse(json.dumps({'prediction': predictions}))
+
