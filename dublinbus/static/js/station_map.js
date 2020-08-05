fae98d783f2f74487943d2051648aeb1ab4bf42f
Evan McDonagh <evan.mc-donagh@ucdconnect.ie>
2020-08-05 14:56:55 +0100

    Fixed datetimepicker formatting bug

--- dublinbus/static/js/station_map.js
+++ dublinbus/static/js/station_map.js
@@ -974,6 +974,8 @@ function calcFare(fareRoutes){
 //send starts, ends in different segments to backend
 function showPrediction(segmentsinfo){
     segmentsinfo[0]['initialdeparture'] = document.getElementById('datetimepicker1').value;
+
+    // console.log(document.getElementById('datetimepicker1').value);
     // Add nearest stopmarkers to segmentsinfo
     for (var i=0; i<segmentsinfo.length; i++) {
         if (segmentsinfo[i].travelmode == 'TRANSIT') {
