3f37f1e1c7d314aa2127e6be463a136b9b20319a
Evan McDonagh <evan.mc-donagh@ucdconnect.ie>
2020-08-11 14:42:25 +0100

    Add config setup

--- dublinbus/dublinbus/settings.py
+++ dublinbus/dublinbus/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+from dublinbus.config import database_config
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -79,11 +80,11 @@ WSGI_APPLICATION = 'dublinbus.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'user',
-        'USER': 'root',
-        'PASSWORD': 'zxcvbnm,',
-        'HOST': 'localhost',
-        'PORT': '3306',
+        'NAME': database_config['NAME'],
+        'USER': database_config['USER'],
+        'PASSWORD': database_config['PASSWORD'],
+        'HOST': database_config['HOST'],
+        'PORT': database_config['PORT'],
     }
 }
 
@@ -195,3 +196,4 @@ AUTHENTICATION_BACKENDS = [
 ]
 
 SESSION_EXPIRE_AT_BROWSER_CLOSE = True;
+
