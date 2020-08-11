1c310beae6910e698f75cb5c7e28fbe0425d3b66
lgs <leeguosheng@126.com>
2020-08-11 02:56:20 +0100

    Revert "Merge branch 'dev' into route_planing"
    This reverts commit a633ea2503b89545c867189046a1a86f3e6b223d, reversing
    changes made to 87ada94bac8218ea3d18602e47807b3aac70a47f.

--- dublinbus/app01/views.py
+++ dublinbus/app01/views.py
@@ -419,12 +419,8 @@ def showprediction(request):
 
                 try:
                     prediction = get_prediction.get_prediction(route, 1, datestring, stopB, stopA)
-                    if prediction < 0:
-                        prediction = get_prediction.get_prediction(route, 1, datestring, stopA, stopB)
                 except IndexError as e:
                     prediction = get_prediction.get_prediction(route, 2, datestring, stopB, stopA)
-                    if prediction < 0:
-                        prediction = get_prediction.get_prediction(route, 2, datestring, stopA, stopB)
 
                 if prediction != None:
                     prediction = int(prediction)
